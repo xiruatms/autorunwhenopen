d2cfa706ff62307b636eda2163a5fdddc74223b9
Xiangmin Ru <xiru@microsoft.com>
2024-08-22 17:47:42 +0800

    First commit.

---
+++ src/taskpane/taskpane.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, Word */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Word) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+    Office.addin.setStartupBehavior(Office.StartupBehavior.load);
+    Word.run(async (context) => {
+      context.document.body.insertParagraph("This paragraph was automatically inserted.", Word.InsertLocation.end);
+      await context.sync();
+    });
+  }
+});
+
+export async function run() {
+  return Word.run(async (context) => {
+    /**
+     * Insert your Word code here
+     */
+
+    // insert a paragraph at the end of the document.
+    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+    // change the paragraph color to blue.
+    paragraph.font.color = "blue";
+
+    await context.sync();
+  });
+}
